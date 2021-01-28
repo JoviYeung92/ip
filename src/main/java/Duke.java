e581505e43426c053a378ef9e146bb33b49481d9
JoviYeung92 <joviyeung1992@gmail.com>
2021-01-28 20:01:51 +0800

    Level-0 new

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
 public class Duke {
     public static void main(String[] args) {
+
         System.out.println("Hello! I'm Duke\nWhat can i do for you?\nBye. Hope to see you again soon!\n");
+
     }
 }
